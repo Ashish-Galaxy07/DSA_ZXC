534b9093603ed526f60074ad4d8f7fda14cb63f8
Ashish <140075193+Ashish-Galaxy07@users.noreply.github.com>
2026-09-02 19:49:12 +0530

    Time: 0 ms (100%), Space: 45.4 MB (5.31%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        //we can always create another array nums2 where 
+        //all elements are either odd or all are even using the given method
+
+        return true;
+    }
+}
